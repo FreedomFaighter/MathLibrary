7958dff9f6b2f9264904ab298259f877f828323a
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-03-13 08:44:14 -0400

    Added return path where a is zero and you tried to solve it

--- DiffEq/DiffEq.py
+++ DiffEq/DiffEq.py
@@ -1,11 +1,14 @@
 from sympy.solvers import solve
 from sympy import x, y, Symbol
 from math import exp
+from sympy import DiracDelta, oo
 
 def secondOrderSolutionFrom(a, b, c):
+	x = Symbol('x')
 	if(a!=0):
-		x = Symbol('x')
 		roots = solve(a*x**2+b*x+c)
 		c1 = Symbol('c1')
 		c2 = Symbol('c2')
 		return c1*exp(roots[1]*x)+c2*exp(roots[2]*x)
+	else:
+		return (c1+c2)*exp(oo*x)
