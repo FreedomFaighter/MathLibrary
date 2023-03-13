b167b5229b4e57bdf4273d824a9292fd4be213b7
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-03-13 09:45:12 -0400

    Two cases of the linear first order equations standard form of a differential equation

--- DiffEq/DiffEq.py
+++ DiffEq/DiffEq.py
@@ -1,10 +1,8 @@
-from aifc import Error
 from sympy.solvers import solve
-from sympy import x, y, Symbol
-from sympy import oo, integrate,exp
+from sympy.abc import x, y, c, Symbol
+from sympy import oo, integrate, exp, Derivative
 
 def secondOrderSolutionFrom(a, b, c):
-	x = Symbol('x')
 	c1 = Symbol('c1')
 	c2 = Symbol('c2')
 	if(a!=0):
@@ -16,9 +14,17 @@ def secondOrderSolutionFrom(a, b, c):
 #		return Error("Not a differential equation of first or second order")
 
 def seperableEquations(Fg, Fp):
-	x = Symbol('x')
-	y = Symbol('y')
-	c = Symbol('c')
-	h = integrate(1 / Fp, y)
-	g = integrate(Fg, x)
-#	return h - g - c
+	integrate(1 / Fp, y) - integrate(Fg, x) - Symbol('c')
+	
+
+def linearFirstOrderEquation(Fa1, Fa0, Fb):
+	if Fa0 is None:
+		integrate(Fb / Fa1) + c
+	else:
+		if Derivative(Fa1) is Fa0:
+			integrate(Fb, x) / Fa1
+
+def standardForm(Fp, Fq):
+	Fmu = exp(integrate(Fp, x))
+	integrate(Fmu*Fq+c, x) / Fmu
+
