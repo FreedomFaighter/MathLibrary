ca5a5ad02c96a95556caf601c4df655b258c7524
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-03-13 09:17:10 -0400

    Added solution for seperable equations of g(x) and p(y)

--- DiffEq/DiffEq.py
+++ DiffEq/DiffEq.py
@@ -1,7 +1,7 @@
+from aifc import Error
 from sympy.solvers import solve
 from sympy import x, y, Symbol
-from math import exp
-from sympy import DiracDelta, oo
+from sympy import oo, integrate,exp
 
 def secondOrderSolutionFrom(a, b, c):
 	x = Symbol('x')
@@ -10,5 +10,15 @@ def secondOrderSolutionFrom(a, b, c):
 	if(a!=0):
 		roots = solve(a*x**2+b*x+c)
 		return c1*exp(roots[1]*x)+c2*exp(roots[2]*x)
+	elif b!=0:
+		return ((c1+c2)*exp(oo*x),-(c/b)*x)
 	else:
-		return ((c1+c2)*exp(oo*x),-(c/b)*x) 
+		return Error("Not a differential equation of first or second order")
+
+def seperableEquations(Fg, Fp):
+	x = Symbol('x')
+	y = Symbol('y')
+	c = Symbol('c')
+	h = integrate(1 / Fp, y)
+	g = integrate(Fg, x)
+	return h - g - c
