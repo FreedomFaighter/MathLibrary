d598901dcd333b0fee96cee6f1d2cb8b0634dd5a
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-03-16 14:05:17 -0400

    Update DiffEq.py

--- DiffEq/DiffEq.py
+++ DiffEq/DiffEq.py
@@ -7,18 +7,15 @@ from sympy import oo, integrate, exp, Derivative
 def secondOrderSolutionFrom(a, b, c):
 	c1 = Symbol('c1')
 	c2 = Symbol('c2')
-	if(a!=0):
-		roots = solve(a*x**2+b*x+c)
-		return c1*exp(roots[1]*x)+c2*exp(roots[2]*x)
-	elif b!=0:
-		return ((c1+c2)*exp(oo*x),-(c/b)*x)
-#	else:
-#		return Error("Not a differential equation of first or second order")
+	if(a==0):
+			raise Error('not second ordered differential equation')
+	else:
+			roots = solve(a*x**2+b*x+c)
+			return c1*exp(roots[1]*x)+c2*exp(roots[2]*x)
 
 def seperableEquations(Fg, Fp):
 	return integrate(1 / Fp, y) - integrate(Fg, x) - Symbol('c')
 	
-
 def linearFirstOrderEquation(Fa1, Fa0, Fb):
 	if Fa0 is None:
 		return integrate(Fb / Fa1) + c
