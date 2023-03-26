a3ee2133084975bf68f73c4b374ed4c65c8b66a6
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-03-26 23:23:18 +0000

    Removed return statements of final value of differential equation solutions

--- DiffEq/DiffEq.py
+++ DiffEq/DiffEq.py
@@ -11,21 +11,21 @@ def secondOrderSolutionFrom(a, b, c):
 		raise Error('not second ordered differential equation')
 	else:
 		roots = solve(a*x**2+b*x+c)
-	 return c1*exp(roots[1]*x)+c2*exp(roots[2]*x)
+	c1*exp(roots[1]*x)+c2*exp(roots[2]*x)
 
 def seperableEquations(Fg, Fp):
 	return integrate(1 / Fp, y) - integrate(Fg, x) - Symbol('c')
 	
 def linearFirstOrderEquation(Fa1, Fa0, Fb):
 	if Fa0 is None:
-		return integrate(Fb / Fa1) + c
+		integrate(Fb / Fa1) + c
 	else:
 		if Derivative(Fa1) is Fa0:
-			return integrate(Fb, x) / Fa1
+			integrate(Fb, x) / Fa1
 
 def standardForm(Fp, Fq):
 	Fmu = exp(integrate(Fp, x))
-	return integrate(Fmu*Fq+c, x) / Fmu
+	integrate(Fmu*Fq+c, x) / Fmu
 
 def exactEquations(Fmxy, Fnxy):
-	return integrate(Fmxy,x) - integrate(Fnxy-Derivative(integrate(Fmxy,x),y))
+	integrate(Fmxy,x) - integrate(Fnxy-Derivative(integrate(Fmxy,x),y))
