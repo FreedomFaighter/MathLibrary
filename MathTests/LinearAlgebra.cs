fa7a7b18e52822f69551cac9899001a0d642e2c8
John H. Freeman <johnherschelfreeman2013@gmail.com>
2023-07-31 03:57:57 -0400

    Added three new tests to MathTests

--- MathTests/LinearAlgebra.cs
+++ MathTests/LinearAlgebra.cs
@@ -5,11 +5,6 @@ namespace MathTests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Is500by500Matrix()
         {
@@ -17,6 +12,30 @@ namespace MathTests
             Matrix m = new(matrixSize, matrixSize);
 
             Assert.IsTrue(m.Columns == m.Rows && m.Columns == matrixSize && m.Rows == matrixSize);
+        }
+
+        [Test]
+        public void Is1001by1001Matrix()
+        {
+            int matrixSize = 1001;
+            Matrix m = new(matrixSize,matrixSize);
+            Assert.IsTrue(m.Columns == m.Rows && m.Columns == matrixSize && m.Rows == matrixSize);
+        }
+
+        [Test]
+        public void IsNot1000by1000Matrix()
+        {
+            int matrixSize = 1001;
+            Matrix m = new Matrix(matrixSize,matrixSize);
+            Assert.IsFalse((m.Columns * m.Columns) != (1000 ^ 2));
+        }
+
+        [Test]
+        public void IsSquare500by500()
+        {
+            int matrixSize = 500;
+            Matrix matrix = new(matrixSize,matrixSize);
+            Assert.IsTrue((matrixSize ^ 2) == (matrix.Columns * matrix.Rows));
         }
     }
 }
