4a687aa3f4d3b9186a0552dd4461a7be722507a9
John H. Freeman <egot@legacyofthefallen.net>
2025-12-30 19:33:01 -0600

    updated methods to on invoke calculation

--- Statistics/TimeSeries/TimeSeries.cs
+++ Statistics/TimeSeries/TimeSeries.cs
@@ -1,120 +1,101 @@
 using MathLibrary.LinearAlgebra;
+using System.Linq;
 
 namespace Statistics.TimeSeries
 {
-    public class TimeSeries
+    public class TimeSeries : System.Collections.Generic.List<double>
     {
-        private double[] values;
-        private double[] sampleAutocovarianceFunction;
-        private double[] sampleAutocorrelationFunction;
-        private double[] samplePartialAutocorrelationFunction;
-        private double sampleMean, sampleVariance;
-        private Matrix covarianceMatrix;
-        private Matrix autocorrelationMatrix;
-        private Matrix partialAutocorrelationMatrix;
-        //private double Qportmanteautest;
-
-        public double[] SampleAutocorrelationFunction
-        {
-            get { return this.sampleAutocorrelationFunction; }
-        }
-
-        public double[] SamplePartialAutocorrelationFunction
-        {
-            get { return this.samplePartialAutocorrelationFunction; }
-        }
+        
 
         public TimeSeries(double[] v)
         {
             if (v.Length < 1)
-                throw new DivideByZeroException("Using population variance and must have at least 1 observation"); 
-            values = v;
-            sampleMean = values.Mean();
-            sampleVariance = values.Variance(true);
-            ComputeAutocovarianceFunction();
-            ComputeSampleAutocorrelationFunction();
-            ComputeSamplePartialAutocorrelationMatrix();
+                throw new DivideByZeroException("Using population variance and must have at least 1 observation");
+            this.AddRange(v);
         }
 
-        public double this[int i]
+        public double[] ComputeAutocovarianceFunction()
         {
-            get { return this.values[i]; }
-            set { values[i] = value; }
-        }
-
-        private void ComputeAutocovarianceFunction()
-        {
-            this.sampleAutocovarianceFunction = new double[values.Length];
-            for (int h = 0; h < values.Length; h++)
+            double[] sampleAutocovarianceFunction = new double[this.Count];
+            for (int h = 0; h < this.ToArray().Length; h++)
             {
-                this.sampleAutocovarianceFunction[h] = values.Take(values.Length - h).Select((x, t) =>
+                sampleAutocovarianceFunction[h] = this.ToArray().Take(this.Count - h).Select((x, t) =>
                 {
-                    return (values[t + h] - sampleMean) * (values[t] - sampleMean);
-                }).Sum() / values.Length;
+                    return (this[t + h] - this.Average()) * (this[t] - this.ToArray().Variance(true));
+                }).Sum() / this.Count;
             }
 
-            this.covarianceMatrix = new Matrix(this.sampleAutocovarianceFunction.Length, this.sampleAutocovarianceFunction.Length);
-            for (int i = 0; i < this.covarianceMatrix.Values.GetLength(0); i++)
+            Matrix covarianceMatrix = new Matrix(sampleAutocovarianceFunction.Length, sampleAutocovarianceFunction.Length);
+            for (int i = 0; i < covarianceMatrix.Values.GetLength(0); i++)
             {
-                for (int j = 0; j < this.covarianceMatrix.Values.GetLength(1); j++)
+                for (int j = 0; j < covarianceMatrix.Values.GetLength(1); j++)
                 {
-                    this.covarianceMatrix[i, j] = this.sampleAutocovarianceFunction[System.Math.Abs(i - j)];
+                    covarianceMatrix[i, j] = sampleAutocovarianceFunction[System.Math.Abs(i - j)];
                 }
             }
+
+            return sampleAutocovarianceFunction;
         }
 
-        private void ComputeSampleAutocorrelationFunction()
+        public double[] ComputeSampleAutocorrelationFunction()
         {
-            this.sampleAutocorrelationFunction = new double[this.sampleAutocovarianceFunction.Length];
-            for (int h = 0; h < this.sampleAutocovarianceFunction.Length; h++)
+            double[] sampleAutocorrelationFunction = new double[this.Count];
+            double[] sampleAutocovarianceFunction = this.ComputeAutocovarianceFunction();
+
+            for (int h = 0; h < sampleAutocovarianceFunction.Length; h++)
             {
-                this.sampleAutocorrelationFunction[h] = this.sampleAutocovarianceFunction[h] / this.sampleAutocovarianceFunction[0];
+                sampleAutocorrelationFunction[h] = sampleAutocovarianceFunction[h] / sampleAutocovarianceFunction[0];
             }
 
-            this.autocorrelationMatrix = new Matrix(this.sampleAutocorrelationFunction.Length, this.sampleAutocorrelationFunction.Length);
-            for (int i = 0; i < this.autocorrelationMatrix.Values.GetLength(0); i++)
+            Matrix autocorrelationMatrix = new Matrix(sampleAutocorrelationFunction.Length, sampleAutocorrelationFunction.Length);
+            for (int i = 0; i < autocorrelationMatrix.Values.GetLength(0); i++)
             {
-                for (int j = 0; j < this.autocorrelationMatrix.Values.GetLength(1); j++)
+                for (int j = 0; j < autocorrelationMatrix.Values.GetLength(1); j++)
                 {
-                    this.autocorrelationMatrix[i, j] = this.sampleAutocorrelationFunction[System.Math.Abs(i - j)];
+                    autocorrelationMatrix[i, j] = sampleAutocorrelationFunction[System.Math.Abs(i - j)];
                 }
             }
+
+            return sampleAutocorrelationFunction;
         }
 
-        private void ComputeSamplePartialAutocorrelationMatrix()
+        public Matrix ComputeSamplePartialAutocorrelationMatrix()
         {
-            this.partialAutocorrelationMatrix = new Matrix(this.values.Length, this.values.Length);
-            this.samplePartialAutocorrelationFunction = new double[this.values.Length];
-            this.samplePartialAutocorrelationFunction[0] = 1;
-            this.partialAutocorrelationMatrix[0, 0] = 1;
-            this.samplePartialAutocorrelationFunction[1] = this.sampleAutocorrelationFunction[1];
-            this.partialAutocorrelationMatrix[1, 1] = this.sampleAutocorrelationFunction[1];
+            Matrix partialAutocorrelationMatrix = new Matrix(this.Count, this.Count);
+            double[] samplePartialAutocorrelationFunction = new double[this.Count];
+            double[] sampleAutocorrectionFunction = this.ComputeSampleAutocorrelationFunction();
+            samplePartialAutocorrelationFunction[0] = 1;
+            partialAutocorrelationMatrix[0, 0] = 1;
+            samplePartialAutocorrelationFunction[1] = sampleAutocorrectionFunction[1];
+            partialAutocorrelationMatrix[1, 1] = sampleAutocorrectionFunction[1];
             Func<int, double> Pi_tau = delegate (int tau)
             {
-                double numer = this.sampleAutocorrelationFunction[tau], denom = 1;
+                double numer = sampleAutocorrectionFunction[tau], denom = 1;
                 double sum = 0;
                 for (int i = 1; i < tau; i++)
                 {
-                    sum += this.partialAutocorrelationMatrix[tau - 1, i] * this.sampleAutocorrelationFunction[tau - i];
+                    sum += partialAutocorrelationMatrix[tau - 1, i] * sampleAutocorrectionFunction[tau - i];
                 }
                 return (numer - sum) / (denom - sum);
             };
             Func<int, int, double> Pi_tau_j = delegate (int tau, int j)
             {
-                return this.partialAutocorrelationMatrix[tau - 1, j]
-                    - this.partialAutocorrelationMatrix[tau, tau]
-                    * this.partialAutocorrelationMatrix[tau - 1, tau - j];
+                return partialAutocorrelationMatrix[tau - 1, j]
+                    - partialAutocorrelationMatrix[tau, tau]
+                    * partialAutocorrelationMatrix[tau - 1, tau - j];
             };
 
-            for (int i = 2; i < this.samplePartialAutocorrelationFunction.Length; i++)
+            for (int i = 2; i < samplePartialAutocorrelationFunction.Length; i++)
             {
-                this.partialAutocorrelationMatrix[i, i] = Pi_tau(i);
-                this.samplePartialAutocorrelationFunction[i] = this.partialAutocorrelationMatrix[i, i];
+                partialAutocorrelationMatrix[i, i] = Pi_tau(i);
+                samplePartialAutocorrelationFunction[i] = partialAutocorrelationMatrix[i, i];
                 for (int j = 1; j < i; j++)
                 {
-                    this.partialAutocorrelationMatrix[i, j] = Pi_tau_j(i, j);
+                    partialAutocorrelationMatrix[i, j] = Pi_tau_j(i, j);
                 }
             }
+
+            return partialAutocorrelationMatrix;
         }
 
         private void ComputeQportmanteau()
